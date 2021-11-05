6ce7519f9328b6ac2d42f2da7d90458384a98ab0
Ben <benbperry1@gmail.com>
2021-11-04 19:38:52 -0700

    commit tree bfcc5b70340557a68a4a071e77f4195a39d82047

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, string) {
+  return array.filter((element) => {
+    return element.toLowerCase() === string.toLowerCase();
+  });
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter((element) => {
+    return element.startsWith(string);
+  });
+}
+
+function matchName(array, string) {
+  return array.filter(({ name }) => {
+    return name === string;
+  });
+}
